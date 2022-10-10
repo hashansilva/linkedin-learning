6d912bb89a1cf54074800db0b5a2a579e347c717
hashan.silva <hashan.silva@gmail.com>
2022-10-10 19:14:24 +0530

    LL:J0202:Student volunteers

---
+++ java_code_challenges/StudentVolunteers/src/main/java/StudentVolunteers.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StudentVolunteers {
+
+    static List<String> findWithIncompleteEvents(
+            List<String> students,
+            Map<String, List<String>> attendeesMapping) {
+
+        Map<String, Integer> studentsEventMap = students.stream().collect(Collectors.toMap(s -> s, n -> 0));
+
+        attendeesMapping.values().forEach(studentList -> {
+            studentList.stream().filter(studentsEventMap::containsKey).forEach(filteredStudent -> studentsEventMap.put(filteredStudent, studentsEventMap.get(filteredStudent) + 1));
+        });
+
+        return studentsEventMap.entrySet().stream().filter(map -> map.getValue() < 2).map(Map.Entry::getKey).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        List<String> students = List.of("Sally", "Polly", "Molly",
+                "Tony", "Harry");
+
+        Map<String, List<String>> attendeesMapping =
+                Map.of("Farmer's Market",
+                        List.of("Sally", "Polly"),
+                        "Car Wash Fundraiser",
+                        List.of("Molly", "Tony", "Polly"),
+                        "Cooking Workshop",
+                        List.of("Sally", "Molly", "Polly"),
+                        "Midnight Breakfast",
+                        List.of("Polly", "Molly"));
+
+        System.out.println(findWithIncompleteEvents(students,
+                attendeesMapping));
+    }
+
+}
