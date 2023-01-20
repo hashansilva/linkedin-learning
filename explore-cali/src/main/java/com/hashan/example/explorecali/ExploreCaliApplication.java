65e479151545245d6ebb03b04f6f243b03a81240
hashan.silva <hashan.silva@gmail.com>
2023-01-20 21:57:17 +0530

    LL:MIC_SERVICE:C01:project setup

---
+++ explore-cali/src/main/java/com/hashan/example/explorecali/ExploreCaliApplication.java
@@ -0,0 +1,13 @@
+package com.hashan.example.explorecali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliApplication.class, args);
+	}
+
+}
---
+++ explore-cali/src/test/java/com/hashan/example/explorecali/ExploreCaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashan.example.explorecali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreCaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
