be24ef7e99a443ada88adb99f6207c79d9e7197d
hashan.silva <hashan.silva@gmail.com>
2023-05-02 08:41:36 +0530

    LL:TDD:initial commit

---
+++ TDDProject/src/com/hashan/silva/Hangman.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023. Hashan Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ */
+
+package com.hashan.silva;
+
+public class Hangman {
+    public int countAlphabet(String word, char alphabet) {
+        int result = 0;
+        for (char c : word.toCharArray()) {
+            if (c == alphabet) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
